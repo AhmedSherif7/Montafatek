5a0b26dd72ca602db513bcb239999aa0b1bd8b3d
aya-cooder <52712767+aya-cooder@users.noreply.github.com>
2021-09-10 15:05:04 +0200

    login,register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,11 +30,15 @@ import { DialogModule } from 'primeng/dialog';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsComponent } from './components/components.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentsComponent
+    ComponentsComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
