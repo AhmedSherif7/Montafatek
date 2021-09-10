5b5da320e3e5cb37c7186c69d9648b7043e6dd26
Ahmed Sherif <ahmed.sherif701@gmail.com>
2021-09-10 11:49:10 +0200

    Adding prime ng dependencies and editig app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Montagatek';
+export class AppComponent implements OnDestroy {
+  title = 'MONTAGATEK';
+
+  ngOnDestroy() {
+    localStorage.clear();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+import { RatingModule } from 'primeng/rating';
+import { StepsModule } from 'primeng/steps';
+import { HttpClientModule } from '@angular/common/http';
+import { ScrollTopModule } from 'primeng/scrolltop';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
+import { BlockUIModule } from 'primeng/blockui';
+import { AvatarModule } from 'primeng/avatar';
+import { BadgeModule } from 'primeng/badge';
+import { ToastModule } from 'primeng/toast';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MessageModule } from 'primeng/message';
+import { MenubarModule } from 'primeng/menubar';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { SidebarModule } from 'primeng/sidebar';
+import { TimelineModule } from "primeng/timeline";
+import { CardModule } from "primeng/card";
+import { PasswordModule } from "primeng/password";
+import { DividerModule } from "primeng/divider";
+import { DialogModule } from 'primeng/dialog';
+// import { ChartModule } from 'primeng/chart';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +36,31 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    FontAwesomeModule,
+    CarouselModule,
+    ButtonModule,
+    RatingModule,
+    StepsModule,
+    HttpClientModule,
+    ScrollTopModule,
+    BlockUIModule,
+    BadgeModule,
+    ToastModule,
+    BrowserAnimationsModule,
+    MessageModule,
+    MenubarModule,
+    DynamicDialogModule,
+    SelectButtonModule,
+    AvatarModule,
+    SidebarModule,
+    ScrollPanelModule,
+    TimelineModule,
+    CardModule,
+    PasswordModule,
+    DividerModule,
+    DialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
